9aa8cbb9738c111c82035f23da87656f38ed3976
Kristen Vulpetti <kristenvulpetti@Mac.attlocal.net>
2026-08-15 21:37:22 -0500

    Add good night weather greeting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,8 @@ function timeGreeting() {
   const hour = new Date().getHours();
   if (hour < 12) return "GOOD MORNING";
   if (hour < 18) return "GOOD AFTERNOON";
-  return "GOOD EVENING";
+  if (hour < 21) return "GOOD EVENING";
+  return "GOOD NIGHT";
 }
 
 export default function Home() {
